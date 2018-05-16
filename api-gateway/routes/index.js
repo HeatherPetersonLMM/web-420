5c88c1327df5c06751863e2491ccb6c0b4001f54
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-05-16 12:03:09 -0400

    added attribution

--- api-gateway/routes/index.js
+++ api-gateway/routes/index.js
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title:  API Gateway Part II
+; Author: Professor Krasso 
+; Date:   16 May 2018
+; Modified By: Heather Peterson
+; Description: Assignment 2.3
+;===========================================
+*/ 
+
 var express = require('express');
 var router = express.Router();
 
